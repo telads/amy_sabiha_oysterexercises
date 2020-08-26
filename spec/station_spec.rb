7920447bfad7853bd1541f13ce8899a5350018bb
Amy <amee_sk@yahoo.com>
2020-08-26 17:11:15 +0100

    update

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,14 +2,14 @@ require './lib/station.rb'
 
 describe 'A station' do
   it 'has a name' do
-    station = Station.new
+    station = Station.new('Baker Street', 1)
+
     expect(station.name).to eq "Baker Street"
   end
 
-
-
   it 'has a zone' do
-    station = Station.new(zone)
+    station = Station.new('Baker Street', 1)
+
     expect(station.zone).to eq 1
   end
 end
